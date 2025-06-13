badda9d4f2acf5d2df140e95fb515091e7260bf8
Elena Lopez <emloppal@gmail.com>
2025-06-13 14:21:16 +0100

    chore: display data on board

---
+++ client/src/components/JobCard.jsx
@@ -0,0 +1,13 @@
+const JobCard = ({ jobTitle, companyName, location }) => {
+  return (
+    <div className="border border-gray-300 rounded-lg p-2 m-2 bg-gray-100">
+      <div className="text-4xl py-2">{jobTitle}</div>
+      <div className="flex gap-4 justify-between py-2">
+        <div>{companyName}</div>
+        <div>{location}</div>
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
