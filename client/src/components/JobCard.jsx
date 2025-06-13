b9d9a0be9ff1a864f6ed817ffeb1e645deee419e
Elena Lopez <emloppal@gmail.com>
2025-06-13 17:31:11 +0100

    chore: open modal when clicking on job card

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,6 +1,6 @@
 const JobCard = ({ jobTitle, companyName, location }) => {
   return (
-    <div className="border border-gray-300 rounded-lg p-2 m-2 bg-gray-100 w-52">
+    <div className="hover:cursor-pointer border border-gray-300 rounded-lg p-2 m-2 bg-gray-100 hover:bg-gray-200 w-52">
       <div className="text-base font-base py-2">{jobTitle}</div>
       <div className="flex gap-4 justify-between py-2 text-gray-700 text-sm">
         <div>{companyName}</div>
