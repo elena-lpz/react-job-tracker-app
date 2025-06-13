470dc0f31d91616e1adc43dd0572f1ca1998539e
Elena Lopez <emloppal@gmail.com>
2025-06-13 23:02:31 +0100

    feat: add react routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
 import JobBoard from "./components/JobBoard";
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
 
 export default function App() {
   return (
     <>
-      <JobBoard />
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Dashboard />} />
+        <Route path={"/jobboard"} element={<JobBoard />} />
+        <Route path={"/profile"} element={<Profile />} />
+      </Routes>
     </>
   );
 }
